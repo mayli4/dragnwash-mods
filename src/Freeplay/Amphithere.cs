838dbc6938c1ee01844aeea5b74a3fe65e8d448b
Sydney Mayfield <sydsleeper@gmail.com>
2026-09-13 15:18:25 -0400

    init

---
+++ src/Freeplay/Amphithere.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+using System.Reflection;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Freeplay;
+
+[BepInPlugin("com.meshlet.freeplay", "DragnWash Freeplay", "1.0.0")]
+internal sealed class Plugin : BaseUnityPlugin {
+
+}
